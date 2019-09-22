0d93326b3b893e573163ba0dbb01faa419acf7eb
gopinadh <annemgopinadh@gmail.com>
2019-09-22 15:46:22 +0530

    Sample java commit
    Change-Id: Id4f70be1bbcb1f058f2ee3790288138216a00b33
    Signed-off-by: gopinadh <annemgopinadh@gmail.com>

---
+++ java/src/gopijava/Sample.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package gopijava;
+
+/**
+ * @author gopinadh
+ *
+ */
+public class Sample {
+
+	/**
+	 * 
+	 */
+	public Sample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
