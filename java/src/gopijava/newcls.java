bfcd37af05caf7549ec15afd1cbe1daf72689ab6
gopinadh <annemgopinadh@gmail.com>
2019-09-22 17:01:00 +0530

    commit 3 file
    Change-Id: Ifc39fb19cbe560cec4455fb8a05d2f8a32e15d96
    Signed-off-by: gopinadh <annemgopinadh@gmail.com>

---
+++ java/src/gopijava/newcls.java
@@ -0,0 +1,14 @@
+package gopijava;
+
+public class newcls {
+
+	public newcls() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello worls2");
+	}
+
+}
